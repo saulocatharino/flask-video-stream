531147ca03dc9847b35021bb145852adcf0b49e9
Saulo Catharino <saulocatharino@gmail.com>
2021-09-10 15:22:25 -0300

    Update camera.py

--- camera.py
+++ camera.py
@@ -6,57 +6,25 @@ import logging
 logger = logging.getLogger(__name__)
 thread = None
 
-
-class VideoCaptureThreading:
-    def __init__(self, src=0, width=640, height=480):
-        self.src = src
-        self.cap = cv2.VideoCapture(self.src)
-        #self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        #self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        self.grabbed, self.frame = self.cap.read()
-        self.started = False
-        self.read_lock = threading.Lock()
-
-    def set(self, var1, var2):
-        self.cap.set(var1, var2)
-
-    def start(self):
-        if self.started:
-            return None
-        self.started = True
-        self.thread = threading.Thread(target=self.update, args=())
-        self.thread.start()
-        return self
-
-    def update(self):
-        while self.started:
-            grabbed, frame = self.cap.read()
-            with self.read_lock:
-                self.grabbed = grabbed
-                self.frame = frame
-
-    def read(self):
-        with self.read_lock:
-            frame = self.frame.copy()
-            grabbed = self.grabbed
-        return grabbed, frame
-
-    def stop(self):
-        self.started = False
-        self.thread.join()
-
-    def __exit__(self, exec_type, exc_value, traceback):
-        self.cap.release()
+def proccess(frame):
+        copy = frame.copy()
+        gray = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
+        mask = backSub.apply(frame)
+        gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+        masked = cv2.bitwise_and(frame,frame, mask=mask)
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        out1 = cv2.vconcat([frame,mask])
+        out2 = cv2.vconcat([gray,masked])
+        out = cv2.hconcat([out1,out2])
+        return out
 
 
 class Camera:
-	def __init__(self,fps=20,video_source='rtmp://x.x.x.x:1935/live'):
+	def __init__(self,fps=30,video_source='rtmp://x.x.x.x:1935/live'):
 		logger.info(f"Initializing camera class with {fps} fps and video_source={video_source}")
 		self.fps = fps
 		self.video_source = video_source
-		self.camera = VideoCaptureThreading(self.video_source)
-		self.camera.start()
-#		self.camera = cv2.VideoCapture(self.video_source)
+		self.camera = cv2.VideoCapture(self.video_source)
 		# We want a max of 5s history to be stored, thats 5s*fps
 		self.max_frames = 5*self.fps
 		self.frames = []
@@ -77,6 +45,8 @@ class Camera:
 		logger.debug("Observation started")
 		while self.isrunning:
 			v,im = self.camera.read()
+			im = proccess(im)
+
 			if v:
 				if len(self.frames)==self.max_frames:
 					self.frames = self.frames[1:]
@@ -87,6 +57,7 @@ class Camera:
 	def stop(self):
 		logger.debug("Stopping thread")
 		self.isrunning = False
+
 	def get_frame(self, _bytes=True):
 		if len(self.frames)>0:
 			if _bytes:
